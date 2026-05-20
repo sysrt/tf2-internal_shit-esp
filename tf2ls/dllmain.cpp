f21167d0409e1dacf555cac75cbc8084dbe41c00
sysrt <bloodyhunterrw@gmail.com>
2026-05-20 14:00:12 +0400

    Update dllmain.cpp

--- tf2ls/dllmain.cpp
+++ tf2ls/dllmain.cpp
@@ -3,7 +3,7 @@
 #include "menu.hpp"
 
 BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved){
-    if (DLL_PROCESS_ATTACH) {
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
         DisableThreadLibraryCalls(hModule);
         HANDLE hM = CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)InitMenu, hModule, 0, nullptr);
         CloseHandle(hM);
